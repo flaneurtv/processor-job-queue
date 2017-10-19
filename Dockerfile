258ca08e06c41d2d42b993d5437bf34ddd7e8631
Markus Kienast <mark@trickkiste.at>
2017-10-19 23:33:54 +0200

    main parts tested

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,10 @@
-FROM node:alpine as builder
+FROM python:3.6-alpine3.4 as builder
 
-RUN apk add --no-cache gcc g++ make python gettext
-RUN yarn install
+RUN apk add --no-cache gcc g++ make python gettext jq coreutils
+
+WORKDIR /usr/src/app
+COPY requirements.txt ./
+RUN pip install --no-cache-dir -r requirements.txt
+COPY . .
+
+CMD [ "python", "/usr/src/app/processor" ]
--- test.sh
+++ test.sh
@@ -1,8 +1,37 @@
-#/!bin/ash
-FILENAME=$1
+#/!bin/bash
+### USAGE ###
+# ./test.sh test.json [in]
+# If you want to debug the input messages and want to see them nicely formatted
+# just add "in" to the end of the test command:  
+# ./test.sh test.json in
+#
+# To see the output of your service-core, just run the test command like this:
+# ./test.sh test.json
+
+# generate test.mp4 with this command:
+# ffmpeg -f lavfi -i testsrc=duration=60:size=1280x720:rate=25 -f lavfi -i sine=frequency=220:beep_factor=4:duration=60:sample_rate=48000 -c:v libx264 -c:a libfdk_aac -y test.mp4
+
+#export SERVICE_UUID=WORKER00-1285-4E4C-A44E-AAAABBBB0000 
+#export SERVICE_NAME=micro-worker-ffmpeg 
+#export SERVICE_HOST=worker00 
+#export NAMESPACE_LISTENER=flaneur 
+#export NAMESPACE_PUBLISHER=flaneur
+
+TESTFILE=$1
+IN=$2
+case $IN in
+  in) PROCESSOR="cat -" ;;
+  *) PROCESSOR="./processor" ;;
+esac
 
 while read line
 do
-    sleep 0.2
-    echo $line | NOW=`date -u +"%FT%T.000Z"` envsubst
-done <$FILENAME | tee /dev/stderr | SERVICE_UUID=SSSSSSSS-1285-4E4C-A44E-AAAABBBB0000 SERVICE_NAME=job_queue SERVICE_HOST=machine NAMESPACE_LISTENER=flaneur NAMESPACE_PUBLISHER=flaneur node processor.js
+  case $line in
+    sleep*) 
+      sleep `echo $line | tr -s ' ' | cut -d ' ' -f 2` 
+      ;;
+    *) 
+      echo $line | CREATED_AT=`date -u +"%FT%T.%NZ"` envsubst 
+      ;;
+  esac
+done < $TESTFILE | $PROCESSOR | jq '.'
